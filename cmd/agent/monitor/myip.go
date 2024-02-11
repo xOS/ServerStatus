6c2fb545f41526a5d1b39597b54382b30652555f
翠花 <git@nange.cn>
2024-02-11 09:31:31 +0800

    Update.

--- cmd/agent/monitor/myip.go
+++ cmd/agent/monitor/myip.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/serverstatus/pkg/utils"
+	"github.com/xos/serverstatus/pkg/utils"
 )
 
 const MacOSChromeUA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
